de7b4f277337502d1ae9a964185614d9145d447f
fbence02 <farkasbence02@gmail.com>
2025-12-01 14:27:37 +0100

    added some "normal" difficulty custom words and random word-choosing logics, modified some css and html

--- scrips.js
+++ scrips.js
@@ -1,1 +1,9 @@
-let words = ["Sigma", "Beta", "Delta", "Alpha", "Epsilon"];
+let wordsNormal = ["ALMA", "KÖRTE", "HÁZTETŐ", "TELEFON", "PROGRAM", "ESERNYŐ", "KANAPÉ"];
+
+let guessedLetters = []
+
+document.getElementById("new-game").addEventListener("click", (e) => {
+  const randIndex = Math.floor(Math.random() * wordsNormal.length);
+  const chosen = wordsNormal[randIndex];
+  document.getElementById("generated-word").textContent = chosen;
+});

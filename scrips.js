3ee3b1510a2c9bac003aec786538bbde6ee935e1
fbence02 <farkasbence02@gmail.com>
2025-12-04 13:17:26 +0100

    worked on a lot of things. Formatted the site to look better. Added a lot of js code the game itself started working but it's still far from the finish line.

--- scrips.js
+++ scrips.js
@@ -1,9 +1,34 @@
-let wordsNormal = ["ALMA", "KÖRTE", "HÁZTETŐ", "TELEFON", "PROGRAM", "ESERNYŐ", "KANAPÉ"];
+let wordsNormal = ["TELEFON", "ALMA", "ARANY", "ABLAK", "RÉPA", "LABRADOR"];
+let wordsHard = ["HÁZTETŐ", "PROGRAM", "ESERNYŐ", "KANAPÉ", "KAUKÁZUS", "JÓS", "NEUROTRANSZMITTER", "RADIÁTOR", "NYÚL", "KEFÍR", "MOSONMAGYARÓVÁR"];
 
 let guessedLetters = []
+let chosen;
+let unknownWord;
 
 document.getElementById("new-game").addEventListener("click", (e) => {
   const randIndex = Math.floor(Math.random() * wordsNormal.length);
-  const chosen = wordsNormal[randIndex];
-  document.getElementById("generated-word").textContent = chosen;
+  chosen = wordsHard[randIndex];
+  unknownWord = "_".repeat(chosen.length);
+
+  document.getElementById("generated-word").textContent = unknownWord;
+});
+
+document.getElementById("enter-button").addEventListener("click", (e) => {
+  const letter = document.getElementById("letter-input").value;
+  
+  console.log(letter)
+
+  for (let i = 0; i < chosen.length; i++) {
+    if (chosen[i] === letter.toUpperCase()) {
+      unknownWord = unknownWord.substring(0, i) + letter.toUpperCase() + unknownWord.substring(i + 1);
+    }
+  }
+
+  document.getElementById("generated-word").textContent = unknownWord;
+
+  document.getElementById("letter-input").value = "";
+
+  if (unknownWord === chosen) {
+    alert("NYERTÉL!\nA szó " + chosen + " volt.");
+  }
 });

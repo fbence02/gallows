38c0b915ce5ed0ff3136382712b4060f2e73dceb
fbence02 <farkasbence02@gmail.com>
2025-12-08 16:04:00 +0100

    Added images to represent the hanging procedure, (the images will be modified). Added more logics to the code and modified its look. New imagereplacer function

--- scrips.js
+++ scrips.js
@@ -1,21 +1,36 @@
 let wordsNormal = ["TELEFON", "ALMA", "ARANY", "ABLAK", "RÉPA", "LABRADOR"];
-let wordsHard = ["HÁZTETŐ", "PROGRAM", "ESERNYŐ", "KANAPÉ", "KAUKÁZUS", "JÓS", "NEUROTRANSZMITTER", "RADIÁTOR", "NYÚL", "KEFÍR", "MOSONMAGYARÓVÁR"];
+let wordsHard = ["HÁZTETŐ", "PROGRAM", "ESERNYŐ", "KANAPÉ", "KAUKÁZUS", "JÓS", "NEUROTRANS5ZMITTER", "RADIÁTOR", "NYÚL", "KEFÍR", "MOSONMAGYARÓVÁR"];
 let guessedLetters = [];
 let randomWord = "";
 let unknownWord = "";
 let maxGuesses = 0;
+let inGame = false;
+let hangerImage = document.getElementById("hanger-image")
+let faultCounter = document.getElementById("faults")
 
 document.getElementById("new-game").addEventListener("click", () => {
-  const randIndex = Math.floor(Math.random() * wordsNormal.length);
-  randomWord = wordsNormal[randIndex];
+  const randIndex = Math.floor(Math.random() * wordsHard.length);
+  randomWord = wordsHard[randIndex];
   unknownWord = "_".repeat(randomWord.length);
   document.getElementById("generated-word").textContent = unknownWord;
   guessedLetters = [];
   document.getElementById("wrong-letters").textContent = "";
   document.getElementById("letter-input").value = "";
+  hangerImage.src = "";
+  faultCounter.textContent = "11/0";
+  inGame = true;
 });
 
+function hangerImageReplacer(maxGuesses) {
+  hangerImage.src = `./images/akasztofa_${maxGuesses}.png`;
+  faultCounter.textContent = "11/" + maxGuesses;
+}    
+    
 function guessHandler() {
+  if (!inGame) {
+    alert("nem vagy játékban!");
+    return;
+  } 
   const letter = document.getElementById("letter-input").value.trim().toUpperCase();
   if (!letter || guessedLetters.includes(letter)) {
     document.getElementById("letter-input").value = "";
@@ -34,6 +49,13 @@ function guessHandler() {
 
   if (!found) {
     document.getElementById("wrong-letters").textContent += letter + " ";
+    maxGuesses += 1;
+    hangerImageReplacer(maxGuesses);
+    if (maxGuesses === 11) {
+      alert("Vesztettél, a szó " + randomWord + " volt.");
+      maxGuesses = 0;
+      inGame = false;
+    }
   }
 
   document.getElementById("generated-word").textContent = unknownWord;
@@ -43,6 +65,7 @@ function guessHandler() {
     alert("NYERTÉL!\nA szó " + randomWord + " volt.");
     guessedLetters = [];
     document.getElementById("wrong-letters").textContent = "";
+    maxGuesses = 0;
   }
 }
 
